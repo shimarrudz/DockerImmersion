8639ad4adc5ae773226ffc66764883497155e558
shimarrudz <vic.shima.vss@gmail.com>
2023-07-03 15:55:53 -0300

    feat: explain file

---
+++ dockerExplain.js
@@ -0,0 +1,14 @@
+class Imagem {
+    
+    programas
+    variaveis_ambiente
+    aplicação
+}
+// A imagem é um molde que define um ambiente em que um conteriner será executado
+
+const container1 = new Imagem();
+const container2 = new Imagem();
+const container3 = new Imagem();
+const container4 = new Imagem();
+const container5 = new Imagem();
+const container6 = new Imagem();
